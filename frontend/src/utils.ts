683a0c5ae5adc7dca25069b29949ea9473bd2157
gerasim13 <gerasim13@gmail.com>
2023-12-29 23:03:57 +0300

    Update of gitignore; Reformat of utils.ts;

--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -1,18 +1,18 @@
 export function secondsToHHMMSS(seconds: number): string {
-  const format: any = (value: number) => (value < 10 ? `0${value}` : value);
-  var hours: number = Math.floor(seconds / 3600);
-  var minutes: number = Math.floor((seconds - (hours * 3600)) / 60);
-  seconds %= 60;
-  return `${format(hours)}:${format(minutes)}:${format(seconds)}`;
+	const format: any = (value: number) => (value < 10 ? `0${value}` : value);
+	var hours: number = Math.floor(seconds / 3600);
+	var minutes: number = Math.floor((seconds - (hours * 3600)) / 60);
+	seconds %= 60;
+	return `${format(hours)}:${format(minutes)}:${format(seconds)}`;
 }
 
 export function randomEnum<T>(anEnum: T, maxIndex?: number): T[keyof T] {
-  const enumValues = (Object.values(anEnum) as unknown) as T[keyof T][];
-  const randomIndex = Math.round(Math.random() * (maxIndex ?? enumValues.length));
-  return enumValues[randomIndex];
+	const enumValues = (Object.values(anEnum) as unknown) as T[keyof T][];
+	const randomIndex = Math.round(Math.random() * (maxIndex ?? enumValues.length));
+	return enumValues[randomIndex];
 }
 
 export function enumIndex<T>(anEnum: T, enumValue: T[keyof T]): number {
-  const enumValues = (Object.values(anEnum) as unknown) as T[keyof T][];
-  return enumValues.indexOf(enumValue);
+	const enumValues = (Object.values(anEnum) as unknown) as T[keyof T][];
+	return enumValues.indexOf(enumValue);
 }
